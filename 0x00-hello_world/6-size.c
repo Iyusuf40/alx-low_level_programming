d32b5788a69cd8e0feace3f8412c8b6dda690e14
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-12 14:59:06 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
 	printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
