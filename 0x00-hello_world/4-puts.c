95eace58d3e799569873284b33e92c857d103e94
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-12 14:51:30 +0000

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * ndkb
+ * main - ndkb
+ *
+ * return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
