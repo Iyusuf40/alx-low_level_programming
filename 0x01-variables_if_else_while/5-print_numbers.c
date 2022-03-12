35798947fa5de7603905a7cb55420a102a20ee3b
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-12 15:13:04 +0000

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(void)
+/**
+ * main
+ *
+ * Return
+ */
+int main(void) /*main*/
 {
 	int i;
+
 	i = 0;
 
-	while (i < 10){
+	while (i < 10)
+	{
 		printf("%d", i);
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
