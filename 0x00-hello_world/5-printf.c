d6f2f650af5b81f10dc55b702706236be227ace2
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-12 14:31:38 +0000

    t

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*this
- *is
- main
+/*
+ * this
+ *ismain
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
