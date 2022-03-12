7cf7979903fc59e5643bac29b9125179e60460be
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-12 14:04:09 +0000

    *rld

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+/*this is my main*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
