e1fdd47e8d1b124dc131d8672278c45a6c88a506
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-12 14:57:22 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - does main
+ *
+ * Return: 0 if
+ */
 int main(void)
 {
 	char c;
@@ -6,12 +12,10 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
-
 	printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
 	printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (int)sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(lli));
 	printf("Size of a float: %d byte(s)\n", (int)sizeof(f));
-
-	return(0);
+	return (0);
 }
