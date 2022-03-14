3951524a2f45fae7e8eb2b51f90914593d61c799
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-14 14:39:33 +0000

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,39 +6,38 @@
 */
 int main(void)
 {
-  int i;
+	int i;
 	int j;
 
-	i='0';
-	j='1';
+	i = '0';
 
-  while (i <= '9')
+	while (i <= '9')
 	{
-	  j = '1';
+		j = '1';
 		while (j <= '9')
 		{
-		  if (i != j)
-		  {
-		      if (j > i)
-		      {
-		          putchar(i);
-		          putchar(j);
-		      }
-		  }
+			if (i != j)
+			{
+		      		if (j > i)
+		      		{
+		          		putchar(i);
+		          		putchar(j);
+		      		}
+		  	}
 			if ((i != '8') || (j != '9'))
 			{
-			     if (i != j)
-			     {
-			          if (j > i)
-			          {
-		                  putchar(',');
-			                putchar(' ');
-			          }
-			     }
+				if (i != j)
+			     	{
+			        	if (j > i)
+			          	{
+		                  		putchar(',');
+			                	putchar(' ');
+			          	}
+			     	}
 			}
 			j++;
 		};
-	  i++;
+	  	i++;
 	};
 	putchar('\n');
 	return (0);
