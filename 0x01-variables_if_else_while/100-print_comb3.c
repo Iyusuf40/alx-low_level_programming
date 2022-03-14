6a88f23112d5c6d9e5f526c831a49b3663fca1b8
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-14 14:28:07 +0000

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- does numbers
+ *Return: 0 if success
+*/
+int main(void)
+{
+  int i;
+	int j;
+
+	i='0';
+	j='1';
+
+  while (i <= '9')
+	{
+	  j = '1';
+		while (j <= '9')
+		{
+		  //putchar(i);
+		  //putchar(j);
+		  if (i != j)
+		  {
+		      if (j > i)
+		      {
+		          putchar(i);
+		          putchar(j);
+		      }
+		  }
+			if ((i != '8') || (j != '9'))
+			{
+			     if (i != j)
+			     {
+			          if (j > i)
+			          {
+		                  putchar(',');
+			                putchar(' ');
+			          }
+			     }
+			}
+			j++;
+		};
+	  i++;
+	};
+	putchar('\n');
+	return (0);
+}
