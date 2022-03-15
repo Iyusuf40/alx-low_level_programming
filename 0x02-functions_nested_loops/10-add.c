9ed64b8ce128acbbb5a67ffc3d266f3c185cfad0
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-15 22:01:33 +0000

    sev

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sums 2 numbers
+ * @i - is first digit
+ * @j - is second digit to add to a
+ * Return : sum of the numbers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
