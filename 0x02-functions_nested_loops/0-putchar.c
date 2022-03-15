7b701a13fe96c8b1f26c304edc6ec048e7c6998c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-15 14:00:23 +0000

    0 and readme

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - employs _putchar() func
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char i = '_', j = 'p', k = 'u', l = 't';
+	char m = 'c', n = 'h', o = 'a', p = 'r';
+
+	_putchar(i);
+	_putchar(j);
+	_putchar(k);
+	_putchar(l);
+	_putchar(m);
+	_putchar(n);
+	_putchar(o);
+	_putchar(p);
+	_putchar('\n');
+	return (0);
+}
