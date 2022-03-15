35ca3346f333164f4e6a470c4ae6f2525a9558e7
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-15 17:22:17 +0000

    several

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints lower
+ * Return: void if successful
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int loop = 0;
+
+	while (loop < 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		loop++;
+	}	
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - checks if argument is less or more than 0
+ * Return: 1 and print + if positive, 0 if 0 else -1
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
