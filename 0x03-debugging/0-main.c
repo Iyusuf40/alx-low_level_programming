80595c6959b9ef5cdc25fa3cda17907db902983e
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-16 17:56:42 +0000

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0
+* Return: 0 if success
 */
 
 int main(void)
@@ -10,7 +10,6 @@ int main(void)
         int i;
 
         i = 0;
-
         positive_or_negative(i);
 
         return (0);
