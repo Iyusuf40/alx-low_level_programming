51203c7a42597992cd75a88ee31656f70585b919
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-18 14:29:23 +0000

    6

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints seriesd of _
+ * @n: the number of \ to print
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int x = 0;
+
+		while (x < n)
+		{
+			_putchar('\');
+			_putchar('\n');
+			int y = 0;
+
+			while (y < x)
+			{
+				_putchar(' ');
+				y++;
+			}
+			x++;
+		}
+	}
+	_putchar('\n');
+}
