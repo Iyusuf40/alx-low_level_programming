2bc9c5d784451dcdddf2a8e2dc2dd4b0dd1e012c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-18 13:39:38 +0000

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints seriesd of _
+ * @n: the number of _ to print
+ */
+void print_line(int n)
+{
+	if (n != 0)
+	{
+		int x = 0;
+		while(x <= n)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
