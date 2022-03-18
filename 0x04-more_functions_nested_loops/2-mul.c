804d0b0ebd1bf75cfb797dbb2bc14de8f141a3d4
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-18 09:30:05 +0000

    1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - multiplies args
+ * @a: first arg
+ * @b: second arg
+ * Return: res which is multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
