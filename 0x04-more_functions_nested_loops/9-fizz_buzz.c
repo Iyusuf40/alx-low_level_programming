217a72087286c26648459ce4e95448d686316ed3
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-18 20:40:52 +0000

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,9 +18,9 @@ int main(void)
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if ((i % 5 == 0) && (i != 100))
+			printf("Buzz");
+		else if (i % 5 == 0)
 			printf("Buzz ");
-			else
-				printf("Buzz");
 		else
 			printf("%d ", i);
 		i++;
