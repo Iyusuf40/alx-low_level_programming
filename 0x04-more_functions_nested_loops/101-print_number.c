9a596f4f4c5cc8848d728956b3947849334bf704
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-19 09:07:23 +0000

    11

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints char numbers
+ */
+void print_number(int n)
+{
+	int i;
+
+	i = 9;
+
+	if (n >= 10)
+	{
+		while (i > 0)
+		{
+			if ((n / 10) == i)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			} i--;
+		}
+	}
+	else
+		_putchar(n + '0');
+	_putchar('\n');
+}
