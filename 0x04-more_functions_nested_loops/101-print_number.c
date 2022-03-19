27500a68c47a2648b78aef266e6db98cf5bf0a1f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-19 11:45:32 +0000

    11

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,46 +6,45 @@
  */
 void print_number(int n)
 {
-        int i;
+	int i;
 
-        i = 9;
+	i = 9;
 
-        if (n < 0)
-        {
-                n = -n;
-                putchar('-');
-        }
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
 
-        if (n >= 10 && n <= 99)
-        {
-                while (i > 0)
-                {
-                        if ((n / 10) == i)
-                        {
-                                putchar((n / 10) + '0');
-                                putchar((n % 10) + '0');
-                        }
-                        i--;
-                }
-        }
+	if (n >= 10 && n <= 99)
+	{
+		while (i > 0)
+		{
+			if ((n / 10) == i)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			i--;
+		}
+	}
 
-        else if (n >= 100 && n <= 999)
-        {
-                putchar((n / 100) + '0');
-                putchar(((n / 10) % 10) + '0');
-                putchar(((n % 100) % 10) + '0');
-        }
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar(((n % 100) % 10) + '0');
+	}
 
-        else if (n >= 1000 && n <= 9999)
-        {
-                putchar((n / 1000) + '0');
-                putchar(((n / 100) % 10) + '0');
-                putchar(((n / 10) % 10) + '0');
-                putchar(((n % 1000) % 10) + '0');
-        }
+	else if (n >= 1000 && n <= 9999)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar(((n % 1000) % 10) + '0');
+	}
 
-        else
-                putchar(n + '0');
-        putchar('\n');
+	else
+		_putchar(n + '0');
+	_putchar('\n');
 }
-
