4404643370da5fe6a6fd974bdcf619cfd56549fe
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-20 10:19:55 +0000

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,7 +6,19 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n == INT_MIN)
+	{
+		int t;
+		int i;
+
+		_putchar('-');
+		t = -(n / 10000);
+		i = -(n % 10000);
+		print_number(t);
+		print_number(i);
+	}
+
+	if (n < 0 && n != INT_MIN)
 	{
 		n = -n;
 		_putchar('-');
@@ -18,7 +30,7 @@ void print_number(int n)
 		_putchar((n % 10) + '0');
 	}
 
-	else if (n < 10)
+	if (n < 10 && n >= 0)
 	{
 		_putchar(n + '0');
 	}
