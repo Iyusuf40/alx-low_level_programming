29bc7a9ca0111f140a72056686099bd590b16481
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-22 04:19:15 +0000

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rev_string- reverses
+ * @s: par
+ */
+void rev_string(char *s)
+{
+	int max;
+
+	max = _strlen(s) - 1;
+
+	while (max >= 0)
+	{
+		_putchar(*(s + max));
+		max--;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - prints array of chars in rev order
+ * @s: is par
+ * Return: i as len
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
