e5d2cf21a10990e7e6c0b8b12a2834b6f5706c79
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-22 22:46:44 +0000

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,14 @@
 /**
  * _strcpy - copies a str from a to b
  * @dest: first par
- * @src- sec par
+ * @src: sec par
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	char *psrc;
 	int size, index;
-	
+
 	psrc = src;
 	size = 0;
 	index = 0;
