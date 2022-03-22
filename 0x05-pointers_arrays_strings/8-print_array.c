fe1d126cc13b3986e15a3ceab2ff4e20084f61da
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-22 21:57:40 +0000

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array
+ * @a: first par
+ * @n: sec par
+ */
+void print_array(int *a, int n)
+{
+	int z = 0;
+
+	while (z < n)
+		printf("%d, " a[z]);
+	printf("\n");
+}
