a4c420c824611f37fe7b3a11a724c4b80546f207
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-23 21:04:49 +0000

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,79 +1,91 @@
+/**
+ * _pow - expo
+ * @s: first par
+ * @v: sec par
+ * Return: y
+ */
 int _pow(int s, int v)
 {
-  int y;
-  y = 1;
+	int y;
 
-  for (v = v; v > 0; v--)
-  {
-    y = y * s;
-  }
-  return y;
+	y = 1;
+	for (v = v; v > 0; v--)
+	{
+		y = y * s;
+	}
+	return (y);
 }
 
+/**
+ * _atoi - ch to int
+ * @s: first par
+ * Return: x
+ */
 int _atoi(char *s)
 {
-        int len, ilen, slen, i, j, v, y;
+	int len, ilen, slen, i, j, v, y;
 	unsigned int x;
-        char *ss;
-        int *si;
-        char *sss;
+	char *ss;
+	int *si;
+	char *sss;
 	char *sign;
 
-        x = 0;
-        y = 0;
-        ss = s;
+	x = 0;
+	y = 0;
+	ss = s;
 	sign = s;
-        si = &j;
-        len = 0;
-        i = 0;
+	si = &j;
+	len = 0;
+	i = 0;
 	slen = 0;
-        ilen = 0;
-        sss = s;
+	ilen = 0;
+	sss = s;
+	while (*sign != '\0')
+	{
+		if (*sign == '-')
+		{
+			slen++;
+			sign++;
+			if (*sign < '0' || *sign > '9')
+				break;
 
-        while(*sign != '\0')
-        {
-          if (*sign == '-')
-            slen++;
-          sign++;
-        }
-
-        while (*ss != '\0')
-        {
-          ss++;
-          len++;
-        }
-
-        while (*sss != '\0')
-        {
-          if (*sss >= '0' && *sss <= '9')
-          {
-            ilen++;
-            if (*sss < '0' || *sss > '9')
-                break;
-          }
-          sss++;
-        }
-
-        while (i < len)
-        {
-          if (*s >= '0' && *s <= '9')
-          {
-            *si = *s - '0';
-            v = _pow(10, (ilen - 1));
-            y = v * *si;
-            x += y;
-            ilen--;
-            s++;
-            if (*s < '0' || *s > '9')
-                break;
-          }
-          else
-          {
-            s++;
-          }
-          i++;
-        }
-        if (slen % 2 == 1)
-		return (int) -x;
-	return x;
+		}
+	}
+	while (*ss != '\0')
+	{
+		ss++;
+		len++;
+	}
+	while (*sss != '\0')
+	{
+		if (*sss >= '0' && *sss <= '9')
+		{
+			ilen++;
+			sss++;
+			if (*sss < '0' || *sss > '9')
+				break;
+		}
+	}
+	while (i < len)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			*si = *s - '0';
+			v = _pow(10, (ilen - 1));
+			y = v * *si;
+			x += y;
+			ilen--;
+			s++;
+			if (*s < '0' || *s > '9')
+				break;
+		}
+		else
+		{
+			s++;
+		}
+		i++;
+	}
+	if (slen % 2 == 1)
+		return ((int) (-x));
+	return (x);
 }
