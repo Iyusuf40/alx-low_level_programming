06bad18496886e63566830c9796795af1cfb7958
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-24 22:27:35 +0000

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,5 @@
+#include "main.h"
+#include <stdio.h>
 /**
  * reverse_array- rev an array of int
  * @a: 1 par
@@ -10,9 +12,10 @@ void reverse_array(int *a, int n)
 	i = n - 1;
 	while (i >= 0)
 	{
-		if (i != n - 1);
+		if (i != n - 1)
 		{
-			printf(", ");
+			_putchar(',');
+			_putchar(' ');
 		}
 		printf("%d", a[i]);
 		i--;
