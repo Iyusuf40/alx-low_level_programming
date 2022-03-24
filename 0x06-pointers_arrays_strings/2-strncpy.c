793e833ba78192d4319c3e30f6bdadba033b8e98
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-24 20:03:33 +0000

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+/**
+ * _strncpy - copies str
+ * @dest: 1 par
+ * @src: 2 par
+ * @n: 3 par
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j, k, l;
+
+	j = k = l = 0;
+
+	while (*(src + j) != '\0')
+	{
+		j++;
+	}
+
+	while (*(src + k) != *(src + n))
+	{
+		if (l > j)
+		{
+			*(dest + l) = 0;
+		}
+	else
+		*(dest + l) = *(src + k);
+	k++;
+	l++;
+	}
+	return (dest);
+}
