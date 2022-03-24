0cee1d5b36d43cfe11f0e2a3c1a71f126d471305
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-24 22:45:11 +0000

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * pr_ch- rev an array of int
+ *
+ * @n: sec par
+ */
+void pr_ch(int n)
+{
+	if (n < 0)
+		putchar('-');
+	if (n / 10 > 0)
+	{
+		pr_ch(n / 10);
+		putchar((n % 10) + '0');
+	}
+	if (n / 10 == 0)
+		putchar(n + '0');
+}
+
 /**
  * reverse_array- rev an array of int
  * @a: 1 par
@@ -17,7 +35,7 @@ void reverse_array(int *a, int n)
 			_putchar(',');
 			_putchar(' ');
 		}
-		printf("%d", a[i]);
+		pr_ch(a[i]);
 		i--;
 	}
 	printf("\n");
