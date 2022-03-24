a4b243e81133e6e928f2255205055f79657ca2db
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-24 17:35:05 +0000

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,45 @@
+/**
+ * _strncat - ccats
+ * @dest: first par
+ * @src: sec par
+ * @n: third par
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len;
+	char *s;
+
+	s = src;
+	i = len = j = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (*(src + len) != '\0')
+	{
+		len++;
+	}
+
+	if (n <= len)
+	{
+		while (*src != *(s + n))
+		{
+			*(dest + i) = *(src + j);
+			i++;
+			src++;
+		}
+	}
+	else
+	{
+		n = len;
+		while (*src != *(s + n))
+		{
+			*(dest + i) = *(src + j);
+			i++;
+			src++;
+		}
+	}
+	return dest;
+}
