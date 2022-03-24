f70246a99cbe175921964e3e7d77a83534aa83ee
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-24 14:28:34 +0000

    101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,13 +2,9 @@
 #include <stdio.h>
 /**
  * main - generates random numbers
- */ 
+ */
 int main()
 {
-	int key;
-
-	srand(5);
-	key = rand();
 	printf("%s", "n6_;jSoIMp2BU4h;;4t3keKvK=g8Sl|Dw");
 	return (0);
 }
