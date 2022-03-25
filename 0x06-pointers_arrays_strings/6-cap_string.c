15b7cee9e303a8d518fd270ea3d5b9872d2695ef
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-25 02:04:43 +0000

    5

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,20 @@
 /**
- * string_toupper - conv to upper
+ * cap_string - conv str
  * @c: arg
  * Return: c
  */
 char *cap_string(char *c)
 {
 	int i = 0;
+
 	while (*(c + i) != '\0')
 	{
 		if (*(c + i) >= 'a' && *(c + i) <= 'z')
 			if (*(c + i - 1) < '0' || *(c + i - 1) > '9')
-				if((*(c + i - 1) < 'A' || *(c + i - 1) > 'Z') && (*(c + i - 1) < 'a' || *(c + i - 1) > 'z'))
+				if ((*(c + i - 1) < 'A' || *(c + i - 1) > 'Z') &&
+				(*(c + i - 1) < 'a' || *(c + i - 1) > 'z'))
 					*(c + i) = *(c + i) - 32;
-    		i++;
+		i++;
 	}
 	return (c);
 }
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,60 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void print_array(int *a, int n)
+/**
+ * string_toupper - conv to upper
+ * @c: arg
+ * Return: c
+ */
+char *cap_string(char *c)
 {
-    int i;
+	int i = 0;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
+	while (*(c + i) != '\0')
+	{
+		if (*(c + i) >= 'a' && *(c + i) <= 'z')
+			if (*(c + i - 1) < '0' || *(c + i - 1) > '9')
+				if ((*(c + i - 1) < 'A' || *(c + i - 1) > 'Z') &&
+						(*(c + i - 1) < 'a' || *(c + i - 1) > 'z'))
+					*(c + i) = *(c + i) - 32;
+		i++;
+	}
+	return (c);
 }
 
-/**
- * reverse_array- rev an array of int
- * @a: 1 par 
- * @n: sec par
- */
-void reverse_array(int *a, int n)
-{
-    int i, j, k;
-    int w[n];
-    int v[n];
-
-    i = n - 1;
-    j = k = 0;
-    while (k < n / 2)
-    {
-      w[j] = *(a + j);
-      v[j] = *(a + i);
-      *(a + j) = v[j];
-      *(a + i) = w[j];
-      j++;
-      i--;
-      k++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
     return (0);
 }
