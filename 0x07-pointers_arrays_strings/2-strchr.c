e539905b95b910fcd61d1236f6b787d6637298ff
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-28 14:23:20 +0000

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+/**
+ * _strchr- finds a char in s
+ * @s: strng to search
+ * @c: char to find
+ * Return: address of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}
