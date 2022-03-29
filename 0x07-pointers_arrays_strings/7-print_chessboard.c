b5cce1f34d69a6447186f41bac9e89bc30bf8329
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-29 04:12:41 +0000

    6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+/**
+ * print_chessboard - prints chess board
+ * @a: par
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j  == 7)
+				_putchar('\n');
+		}
+	}
+}
