8de0667ae44a84cc47e44039283d20bcca4e0a20
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-29 04:34:22 +0000

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_diagsums- prints
+ * @s: 1 par
+ * @size: size
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, k;
+
+	i = size;
+	j = size;
+
+	printf("yet to do");
+}	
