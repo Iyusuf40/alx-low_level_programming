85633a9a0ef12560ed823e5325870669aad84a2b
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-29 20:12:19 +0000

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,17 +1,27 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+
 /**
- * print_diagsums- prints
- * @s: 1 par
- * @size: size
+ * print_diagsums - prints diagonally
+ * @a: array
+ * @size: size of array
  */
 void print_diagsums(int *a, int size)
 {
-	/*int i, j, k;*/
+	int i, k, l, n, sum1, sum2;
 
-	/*i = size;*/
-	/*j = size;*/
+	l = 0;
+	i = 0;
+	sum1 = 0;
+	sum2 = 0;
+	k = 0;
+	n = size - 1;
 
-	printf("yet to do");
+	for (l = 0; l < size; l++)
+	{
+		sum1 += *(a + i);
+		sum2 += *(a + n + k);
+		i += size + 1;
+		k += size - 1;
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
