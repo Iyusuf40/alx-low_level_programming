62e4050754df7d36babc59134cba79c73f53f050
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-29 02:38:45 +0000

    2

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+/**
+ * _strpbrk - does some weird cmp
+ * @s: first par
+ * @accept: second par
+ * Return: j
+ */
+unsigned char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j, k, l;
+
+	i = j = k = l = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+
+	while (*(accept + l) != '\0')
+		l++;
+
+	while (j < i)
+	{
+		k = 0;
+		while (k < l)
+		{
+			if (*(s + j) == *(accept + k))
+			{
+				return (s + j);
+			}
+			k++;
+		}
+		j++;
+	}
+	return (0);
+}
