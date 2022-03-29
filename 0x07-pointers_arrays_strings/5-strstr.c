9ae80c48b3da23ec86f3ee947c06fe3601f9f673
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-29 02:53:45 +0000

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+/**
+ * _strstr - does some weird cmp
+ * @haystack: first par
+ * @needle: second par
+ * Return: where cpm matches
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, k, l, m;
+
+	i = j = k = l = m = 0;
+
+	while (*(haystack + i) != '\0')
+		i++;
+
+	while (*(needle + l) != '\0')
+		l++;
+
+	while (j < i)
+	{
+		k = 0;
+		while (k < l)
+		{
+			if (*(haystack + j) == *(needle + k))
+			{
+				while (m < l)
+				{
+					if (*(haystack + j + m) == *(needle + k + m))
+						m++;
+					else
+						break;
+				}
+				if (m == l)
+					return (haystack + j);
+			}
+			k++;
+		}
+		j++;
+	}
+	return (0);
+}
