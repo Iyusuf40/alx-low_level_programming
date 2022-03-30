4140088153d55c91c10f06bb31f103654f7e28a3
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-30 12:30:49 +0000

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string
+ * @s: array
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) == '\0')
+	{
+		return;
+	}
+	++i;
+	_print_rev_recursion((s + i));
+	_putchar(*(s + i));
+}
