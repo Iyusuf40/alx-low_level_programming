5b9d90f23a377b23ecc82aaca3f435501ddfdbce
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-30 13:51:55 +0000

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,30 @@
 #include "main.h"
 /**
+ * main_print_rev_recursion - prints a string
+ * @s: array
+ *
+ */
+void main_print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) == '\0')
+	{
+		return;
+	}
+	++i;
+	main_print_rev_recursion((s + i));
+	if (*(s + i) != '\0')
+		_putchar(*(s + i));
+}
+
+/**
  * _print_rev_recursion - prints a string
  * @s: array
  *
  */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-
-	if (*(s + i) == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	++i;
-	_print_rev_recursion((s + i));
-	if (*(s + i) != '\0')
-		_putchar(*(s + i));
+	main_print_rev_recursion(s);
+	putchar('\n');
 }
