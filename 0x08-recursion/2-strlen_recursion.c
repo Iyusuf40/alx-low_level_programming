fdc18246d3f94c74db1ac64de21e56dd4aeabfe5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-03-30 14:23:15 +0000

    1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen_recursion - counts chars
+ * @s: char to check
+ * Return: lenth of str
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) == 0)
+		return (i);
+	i++;
+	_strlen_recursion(s + i);
+}
