17d72b3817bfe6f5e4df9264b27f6d82c3eb8c96
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-01 19:56:20 +0000

    fixing mistake

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the executable
+ * main - prints the number of arguments passed
  * @argc: count of arguments
  * @argv: a pointer to array of pointers
  * Return: 0
