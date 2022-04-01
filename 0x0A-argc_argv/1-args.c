d5f63a758680327276ac8b31161867733bd662d4
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-01 17:34:46 +0000

    a function that prints the no of arguments [assed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the executable
+ * @argc: count of arguments
+ * @argv: a pointer to array of pointers
+ * Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
