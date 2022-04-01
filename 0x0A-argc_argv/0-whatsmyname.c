2571bf6582d8fe48bb3b9b8f0635da56aa67f945
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-01 17:31:03 +0000

    a function that writes its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,9 @@
  * main - prints the name of the executable
  * @argc: count of arguments
  * @argv: a pointer to array of pointers
+ * Return: 0
  */
-int main(int argc, char **argv)
+int main(int __attribute__ ((unused)) argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 
