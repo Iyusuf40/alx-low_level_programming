22aa0a0be2f2bd61c11bf348a7e341ab05aa9fc8
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-01 18:16:33 +0000

    a program that prints the arguments passed to it

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the executable
+ * @argc: count of arguments
+ * @argv: a pointer to array of pointers
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		 printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
