1c13384c6d1eb8c7c3ab3991b9571498b0573c53
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-01 20:33:56 +0000

    a program that adds numbers passed to it as arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds numberss
+ * @argc: number or arguments passed
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
