37805f5011b67216a252fff9451878c7fb6ec9f2
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-05 16:38:12 +0000

    A function that frees all memory allocated during grid creation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - frees up memory initially allocated to create a 2 dim array
+ * @grid: the already existing grid
+ * height: height of the grid
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++
+	}
+}
