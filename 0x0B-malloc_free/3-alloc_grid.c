dc52b01a120128b20ba3f1f3edcb57d154eff63c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-05 15:47:48 +0000

    fix broken function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,39 +12,44 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i = 0, j = 0, h = 0;
+	int i = 0, j = 0, h = 0, k = width, l = 0;
 	int **grid, *gri;
 
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	gri = malloc(width * sizeof(int));
+	gri = malloc(width * height * sizeof(int));
 
 	if (gri == NULL)
+	{
+		free(gri)
 		return (NULL);
+	}
 
 	grid = malloc(height * sizeof(gri));
 
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	while (h < height)
 	{
-		grid[h] = gri;
+		grid[h] = gri + l;
 		h++;
+		l += k;
 	}
 
-        while (i < height)
-        {
-                j = 0;
-                while (j < width)
-                {
-                        grid[i][j] = 0;
-                        j++;
-                }
-                i++;
-        }
-
-
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
 	return (grid);
 }
