5705db80cf7ea8344b22b5d08ddb1c7d2271f9d4
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-05 10:40:14 +0000

    create an _strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * _strdup - copies a string literal an place in a new address
+ * @str: string to copy
+ *
+ * Return: address of copied str
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0, j = 0;
+
+	if (str == 0)
+		return (0);
+
+	while (*(str + i) != 0)
+		i++;
+
+	s = malloc(i * sizeof(char));
+
+	if (s == 0)
+		return (0);
+
+	while (*(str + j) != 0)
+	{
+		s[j] = *(str + j);
+		j++;
+	}
+	return (s);
+}
+
