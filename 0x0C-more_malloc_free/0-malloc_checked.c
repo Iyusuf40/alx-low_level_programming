d01672c18c9f83cebf80c9a7a5792d82a76710cd
Yusuf <96367786+Iyusuf40@users.noreply.github.com>
2022-04-07 14:17:23 +0100

    renamed file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 /**
  * malloc_checked - this function craetes a space in memory using malloc
  * @b: stand in  for size_t return value of sizeof function
- *
+ * Return: address of memory
  */
 void *malloc_checked(unsigned int b)
 {
