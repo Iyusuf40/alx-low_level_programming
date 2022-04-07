e494555d311e53b0cd5ea754851ded6c4a5c2368
Yusuf <96367786+Iyusuf40@users.noreply.github.com>
2022-04-07 14:12:16 +0100

    renamed file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+/**
+ * malloc_checked - this function craetes a space in memory using malloc
+ * @b: stand in  for size_t return value of sizeof function
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	if (malloc(b) == 0)
+		exit(98);
+	return (malloc(b));
+}
