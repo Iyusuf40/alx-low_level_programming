ab0ba12ff93bd5c1cddd5e497df56e7e95ad61ed
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-09 14:27:49 +0000

    fix _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,6 +5,7 @@
  * _calloc- mimmics built incalloc
  * @nmemb: number of elements
  * @size: byte size
+ * Return: ptr
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -13,14 +14,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (0);
-	if (malloc(nmemb * size) == 0)
-		return (0);
+
 	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (0);
 
 	while (i < (size * nmemb))
 	{
 		ptr[i] = 0;
 		i++;
 	}
-	return ((void *)ptr);
+	return (ptr);
 }
