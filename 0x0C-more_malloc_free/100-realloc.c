dc76ffff6ba5bfa2693b43729fe3f6b27bb718ae
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-09 17:29:44 +0000

    a realloc mimic function

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc- a stdlib realloc function mimic
+ * @ptr: source address
+ * @old_size: size of previous memory
+ * @new_size: size of new allocaed memory
+ * Return: new address of newly allocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *pt;
+	int i = 0;
+
+	pt = malloc(new_size);
+	if (pt == 0)
+		return (0);
+	if (ptr == 0)
+		return (pt);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	while (i < old_size && i < new_size)
+	{
+		pt[i] = ptr[i];
+		i++;
+	}
+	free(ptr);
+	return (pt);
+}
