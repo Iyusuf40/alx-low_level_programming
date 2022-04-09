ba7e949ae5e3487106a0f06f6f6e11e1895c786a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-09 03:29:05 +0000

    create my own calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * _calloc- mimmics built incalloc
  * @nmemb: number of elements
@@ -8,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i = 0;
-	void *ptr;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
@@ -19,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	while (i < (nmemb / size))
 	{
 		ptr[i] = 0;
-		i++;
+		i += size;
 	}
 	return (ptr);
 }
