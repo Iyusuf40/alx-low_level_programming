58e420ba9759ac9ca27221822069710cc077351b
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-09 03:22:32 +0000

    create my own calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc- mimmics built incalloc
+ * @nmemb: number of elements
+ * @size: byte size
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	if (malloc(nmemb * size) == 0)
+		return (0);
+	ptr = malloc(nmemb * size);
+
+	while (i < (nmemb / size))
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
