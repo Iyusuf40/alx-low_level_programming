f22bee4f962e22089b2ed234fdb3b38e7a670f0a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-09 02:44:00 +0000

    fix malloc-check

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,20 +5,10 @@
  * @b: stand in  for size_t return value of sizeof function
  * Return: address of memory
  */
-/*
 void *malloc_checked(unsigned int b)
 {
 	if (malloc(b) == 0)
 		exit(98);
 	return (malloc(b));
 }
-*/
-void *malloc_checked(unsigned int b)
-{
-	void *ptr;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
-}
