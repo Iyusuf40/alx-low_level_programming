0cbdd3dbf18f6aa219e61098f57f986d5a91e536
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-10 10:35:52 +0000

    advanced task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program entry
+ * @argc: number of arguments passed
+ * @argv: pointer to addresses of arguments passed
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+}
