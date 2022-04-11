a25231f873459044be6d7b861632b5ad1b51fbc7
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-11 16:38:09 +0000

    a macro that computes absolute value of its parameter

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,16 +1,6 @@
 #ifndef HEADER
 #define HEADER
-/**
- * val- converts negative integers to positive
- * @x: int to convert
- * Return: x
- */
-unsigned int val(int x)
-{
-	if (x < 0)
-		return (-x);
-	return (x);
-}
-#define ABS(x) val(x)
+
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 
 #endif
