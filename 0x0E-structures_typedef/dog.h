6830746ae42e899f6d8cfe6f6aa1f29d7293166c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-11 23:40:15 +0000

    create a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+ * struct dog - a struct of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: this is a struct of a dog
+ */
 struct dog
 {
 	char *name;
