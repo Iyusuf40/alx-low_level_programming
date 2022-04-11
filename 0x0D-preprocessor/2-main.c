537d68bb48fbfce6221c459e29bfddf6bf1dc50f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-11 15:54:14 +0000

    employ __FILE__ macro to print name of exe file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- prints the name of the file used to run the program
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
