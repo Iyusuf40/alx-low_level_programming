776dca51f094ff67e05d2918ab6394b420b4d6e2
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-11 23:55:09 +0000

    a function that initiallizes values of members of a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - initializes the values of a struct
+ * @*d: the struct
+ * @name: the name of the new struct
+ * @age: the age
+ * @owner: the name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
