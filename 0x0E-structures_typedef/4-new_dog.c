754c7c9f346ffb4cfdd7247cb713100e81caf1f8
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-12 12:22:05 +0000

    fix code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int len_name = 0, len_owner = 0, i = 0;
-	char *n_name, *n_owner;
 	dog_t *n_dog;
 
 	n_dog = malloc(sizeof(dog_t));
@@ -21,25 +20,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		len_name++;
 	while (owner[len_owner] != 0)
 		len_owner++;
-	n_name = malloc((len_name) * sizeof(char));
-	if (n_name == 0)
+	(*n_dog).name = malloc((len_name) * sizeof(char));
+	if ((*n_dog).name == 0)
 	{
 		free(n_dog);
 		return (0);
 	}
-	n_owner = malloc((len_owner) * sizeof(char));
-	if (n_owner == 0)
+	(*n_dog).owner = malloc((len_owner) * sizeof(char));
+	if ((*n_dog).owner == 0)
 	{
 		free(n_dog);
-		free(n_name);
+		free((*n_dog).name);
 		return (0);
 	}
 	for (i = 0; i < len_name; i++)
-		n_name[i] = name[i];
+		(*n_dog).name[i] = name[i];
 	for (i = 0; i < len_owner; i++)
-		n_owner[i] = owner[i];
-	(*n_dog).name = n_name;
+		(*n_dog).owner[i] = owner[i];
 	(*n_dog).age = age;
-	(*n_dog).owner = n_owner;
 	return (n_dog);
 }
