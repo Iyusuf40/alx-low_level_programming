ccdd3203e8a696122021eefb3cbdb055799a7de3
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-12 11:31:29 +0000

    fix code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,13 +10,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len_name = 0, len_owner = 0, i = 0;
+	char *n_name, *n_owner;
 	dog_t *n_dog;
 
 	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == 0)
 		return (0);
-	(*n_dog).name = name;
+	while (name[len_name] != 0)
+		len_name++;
+	while (name[len_owner] != 0)
+		len_owner++;
+	n_name = malloc(len_name);
+	if (n_name == 0)
+	{
+		free(n_dog);
+		return (0);
+	}
+	n_owner = malloc(len_owner);
+	if (n_owner == 0)
+	{
+		free(n_dog);
+		free(n_name);
+		return (0);
+	}
+	for (i = 0; i < len_name; i++)
+		n_name[i] = name[i];
+	for (i = 0; i < len_owner; i++)
+		n_owner[i] = owner[i];
+	(*n_dog).name = n_name;
 	(*n_dog).age = age;
-	(*n_dog).owner = owner;
+	(*n_dog).owner = n_owner;
 	return (n_dog);
 }
