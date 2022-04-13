d3639d4155a62889dfb0557a05d548c7d8667e2a
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-13 15:49:49 +0000

    add a function that takes 2 arguments, second being a pointer to a function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
 /**
- * print_name - a function that takes 2 arguments, second argument is a pointer to
- * a function that takes one argument which is the first argument of print_name
+ * print_name - a function that takes 2 arguments, second argument is a pointer
+ * to a function that takes one argument which is the first argument of
+ * print_name
+ *
  * @name: pointer to a string and first argument
- * @f: pointer to a function 
+ * @f: pointer to a function
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == 0)
+		return;
 	f(name);
 }
