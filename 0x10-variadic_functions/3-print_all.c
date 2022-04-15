8d632f1f0ed42b28a7183dfaf29a8503b41e8310
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-15 04:11:31 +0000

    not working

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct r_ptr - struct with char member and pointer to char member
+ * @c: the char
+ * @pt: the pointer
+ */
+typedef struct r_ptr
+{
+	char c;
+	char *pt;
+} ptr_t;
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	char *ptr;
+	va_list ap;
+	ptr_t p[] = {
+		{'c', "%c"},
+		{'i', "%d"},
+		{'s', "%s"},
+		{'f', "%f"}
+	};
+
+	va_start(ap, format);
+
+	while (format[i] != 0)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == p[j].c)
+			{
+				ptr = p[j].pt;
+				printf(ptr, va_arg(ap, int));
+				if (format[i + 1] != 0)
+					printf(", ");
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
+
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stschool");
+    return (0);
+}
