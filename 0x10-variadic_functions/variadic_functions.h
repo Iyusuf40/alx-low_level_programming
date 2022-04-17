c5a8b1f63982024e4116a59afd127b3094956025
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-17 09:31:12 +0000

    test function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,13 +3,17 @@
 
 /**
  * struct r_ptr - struct with char member and pointer to char member
- * @c: the char
- * @pt: the pointer
+ * @f1: receives char * arg
+ * @f2: receives int arg
+ * @f3: receives double arg
+ * @f4: receives int arg
  */
 typedef struct r_ptr
 {
-	char c;
-	char *pt;
+	void (*f1)(char *);
+	void (*f2)(int);
+	void (*f3)(double);
+	void (*f4)(int);
 } ptr_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
