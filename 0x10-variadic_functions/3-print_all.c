eeee17c895adddc9a2f828a753f4e0df67146bf1
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-20 16:58:32 +0000

    fix function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -81,7 +81,7 @@ void print_all(const char * const format, ...)
 
 	va_start(ap, format);
 
-	while (format[i] != 0)
+	while (format != 0 && format[i] != 0)
 	{
 		if (get_func(format[i]) != 0)
 		{
