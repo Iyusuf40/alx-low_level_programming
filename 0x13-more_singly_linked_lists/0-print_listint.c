03e0cb4e64dcef4b46360b2799e3d59d43abaed9
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-25 20:22:04 +0000

    handle NULL

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,10 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 
 	if (h == NULL)
-		return (-1);
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
 	while (h)
 	{
 		printf("%d\n", (*h).n);
