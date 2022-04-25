884aba6d69ef22216aeeb186663dd55cd39a1ca2
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-25 20:35:33 +0000

    handle failed malloc

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	current = *head;
 
