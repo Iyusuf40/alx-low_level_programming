773c47253c5057ebcead140b136efe6a17b86f8d
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-25 17:41:22 +0000

    fix code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	current = *head;
 
-	if (index == 0)
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
