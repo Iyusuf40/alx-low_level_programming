c84e8580d22c01f83abf8bb1265b41bb06d9e798
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-25 15:54:22 +0000

    function inserts node at idx position of the list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at idx
+ * @idx: index to insert new node
+ * @n: field n of new node
+ * @head: head of list
+ *
+ * Return: new node if successful else NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *current, *prev;
+	unsigned int count = 0;
+
+	if (!*head)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	current = *head;
+	while (current)
+	{
+		if (count == idx)
+		{
+			new->next = current;
+			prev->next = new;
+			return (new);
+		}
+		count++;
+		prev = current;
+		current = current->next;
+	}
+	return (NULL);
+}
