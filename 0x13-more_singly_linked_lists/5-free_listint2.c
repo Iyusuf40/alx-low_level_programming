8999067353e6eee8a75c4d7809b5fe6bb9540fd2
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-25 12:38:21 +0000

    free_listint2 frees all nodes in a list and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list and sets head to NULL
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *save;
+
+	while (*head)
+	{
+		save = (*head)->next;
+		free(*head);
+		*head = save;
+	}
+	*head = NULL;
+}
