6dc8ed1c394d2f84315d216450cd3ef24f48f2ab
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-25 15:30:21 +0000

    returns node at index of list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns node at index index
+ * @head: head of list
+ * @index: index to return
+ * Return: node at index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node = NULL;
+	unsigned int count = 0;
+	
+	if (!head)
+		return (NULL);
+
+	node = head;
+	while (node)
+	{
+		if (count == index)
+			return (node);
+		node = node->next;
+		count++;
+	}
+
+	return (NULL);
+}
