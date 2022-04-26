f032bdf56d17a3ed06ee9f880c9233a655995cea
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-26 15:20:00 +0000

    free new if no insertion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,12 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev = current;
 		current = current->next;
 	}
-	if (count == idx)
-	{
-		prev->next = new;
-		new->next = NULL;
-		return (new);
-	}
+
 	free(new);
 	return (NULL);
 }
