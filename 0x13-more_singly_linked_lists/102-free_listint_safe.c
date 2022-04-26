5fa34e3e7030bbcf36976ba93403739a1fe7d3d1
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-26 22:15:09 +0000

    test code

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees list
+ * @h: head of list
+ * Return: number of links
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *save = NULL, *save_n = NULL;
+
+	if (*h == NULL)
+	{
+		return (count);
+	}
+	while (*h)
+	{
+		save = *h;
+		save_n = (**h).next;
+		free(*h);
+		*h = save_n;
+		if (*h > save)
+		{
+			return (count);
+		}
+		count++;
+	}
+	*h = NULL;
+	h = NULL;
+	return (count);
+}
