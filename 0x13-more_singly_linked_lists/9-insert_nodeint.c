faa462a861c0cf04349b48683b4fefb6f928208c
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-26 15:30:28 +0000

    handle last index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,16 +12,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *current, *prev;
 	unsigned int count = 0;
 
-	if (!*head)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	current = *head;
 
-	if (idx == 0)
+	if (idx == 0 || !*head)
 	{
 		new->next = *head;
 		*head = new;
