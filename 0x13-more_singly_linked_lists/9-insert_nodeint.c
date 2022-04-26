3aab6ddf283b1be042660278fa35717a2542dbb5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-26 15:26:50 +0000

    handle last index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev = current;
 		current = current->next;
 	}
-	if (count == idx)
+	if (count + 1 == idx)
 	{
 		prev->next = new;
 		new->next = NULL;
