d46979ced6c143056fd8dd6dd27e4293e10cde40
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-26 13:41:03 +0000

    change int to size_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,10 +8,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (count);
-	}
+	/*
+	*if (h == NULL)
+	*{
+	*	return (count);
+	*}
+	*/
 	while (h)
 	{
 		printf("%d\n", (*h).n);
