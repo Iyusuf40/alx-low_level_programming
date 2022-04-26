5d131146853eb1a0bd1be204e6c3e82ba8996b8e
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-26 22:44:52 +0000

    test

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * find_listint_loop - prints the int field of the list
+ * @head: head of list
+ * Return: node where loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *save = NULL;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (head)
+	{
+		save = head;
+		head = (*head).next;
+		if (head > save)
+		{
+			return (save);
+		}
+	}
+	return (NULL);
+}
