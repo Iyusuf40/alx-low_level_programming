1bb8e57a53dcaa9fecbd62e0143c8ccf41067486
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-27 11:26:10 +0000

    modified 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,36 +1,5 @@
 #include "lists.h"
 /**
- * update_array - updates an array
- * @count: index to update
- * @current: address of current head
- * @arr: array to update
- */
-void update_array(listint_t *arr[], size_t count, listint_t *current)
-{
-	arr[count] = current;
-}
-
-/**
- * _check_array - checks an array
- * @count: index to update
- * @current: address of current head
- * @arr: array to update
- * Return: 0 or 1
- */
-int _check_array(listint_t *arr[], size_t count, listint_t *current)
-{
-	size_t i = 0;
-
-	while (i <= count)
-	{
-		if (arr[i] == current)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-/**
  * find_listint_loop - prints the int field of the list
  * @head: head of list
  * Return: node where loop starts
@@ -38,8 +7,6 @@ int _check_array(listint_t *arr[], size_t count, listint_t *current)
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *save = NULL;
-	size_t count = 0;
-	listint_t *arr[200];
 
 	if (head == NULL)
 	{
@@ -48,13 +15,11 @@ listint_t *find_listint_loop(listint_t *head)
 	while (head)
 	{
 		save = head;
-		update_array(arr, count, save);
 		head = (*head).next;
-		if (_check_array(arr, count, head))
+		if (head >= save)
 		{
 			return (head);
 		}
-		count++;
 	}
 	return (NULL);
 }
