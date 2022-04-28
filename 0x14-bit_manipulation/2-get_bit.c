532969c7c54d85e8d99307ebce71257255665a31
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-28 21:38:39 +0000

    add file 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,10 +17,13 @@ int get_bit(unsigned long int n, unsigned int index)
 		len++;
 	}
 
+	if (n == 0 && index == 0)
+		return (0);
+
 	if (index >= len)
 		return (-1);
 
-	if((n & (1 << index)))
+	if ((n & (1 << index)))
 		return (1);
 	return (0);
 }
