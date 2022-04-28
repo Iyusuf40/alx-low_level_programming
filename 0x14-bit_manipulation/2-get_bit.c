698413a608504dfeb43fbd558348f918255bf7f5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-28 21:35:34 +0000

    add file 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_bit - gets bit at index
+ * @n: number to check
+ * @index: index to check
+ *
+ * Return: 1 or 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int copy = n;
+	unsigned int len = 0;
+
+	while (copy)
+	{
+		copy = copy >> 1;
+		len++;
+	}
+
+	if (index >= len)
+		return (-1);
+
+	if((n & (1 << index)))
+		return (1);
+	return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ unsigned int binary_to_uint(const char *b);
 #include <stdio.h>
 #include <stdlib.h>
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
