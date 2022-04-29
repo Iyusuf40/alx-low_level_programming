6e790b6e8a344efa1833218e590dd989a6f7215d
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-29 09:53:07 +0000

    handle index > sizeof variable and update header file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * clear_bit - gets bit at index
+ * @n: number to check
+ * @index: index to check
+ *
+ * Return: 1 or 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((*n & (1 << index)))
+	{
+		*n = *n | (1 << index);
+	}
+	return (1);
+}
