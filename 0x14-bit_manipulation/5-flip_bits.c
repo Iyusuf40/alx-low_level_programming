856a486fec83c4a90cb39a6baff89c63de4054d7
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-04-29 10:48:55 +0000

    add file 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - performs an or opearation on n and m
+ * @n: operand 1
+ * @m: operand 2
+ * Return: diff_len
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff = 0;
+	unsigned int diff_len = 0;
+
+	diff = m | n;
+
+	while (diff)
+	{
+		diff_len++;
+		diff = diff >> 1;
+	}
+	return (diff_len);
+}
