c8f760d3bc832076c63acb0ae65bddfba3264039
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-05-03 23:54:12 +0000

    close fd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,9 +24,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (ret == -1)
 	{
-		/*free(buff);
+		free(buff);
 		close(fd);
-		*/return (0);
+		return (0);
 	}
 	buff[letters] = '\0';
 
@@ -35,6 +35,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ret = write(STDOUT_FILENO, buff, strlen(buff));
 
 	free(buff);
-
+	close(fd);
 	return (ret);
 }
