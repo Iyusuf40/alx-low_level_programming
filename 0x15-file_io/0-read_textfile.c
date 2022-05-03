d9e06d6a2a928b49d9d9eebd10c18ce891f99375
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-05-03 23:09:45 +0000

    read and write from a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - reads filename to a buffer
+ * @filename: file
+ * @letters: bytes to read
+ * Return: number of bytes read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t ret;
+	char *buff;
+	int fd;
+
+	fd = open(filename, O_RDWR);
+	if (filename == NULL || fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+
+	ret = read(fd, buff, letters);
+
+	if (ret == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	buff[letters] = '\0';
+
+	ret = 0;
+
+	ret = write(1, buff, letters);
+
+	free(buff);
+
+	return (ret);
+}
