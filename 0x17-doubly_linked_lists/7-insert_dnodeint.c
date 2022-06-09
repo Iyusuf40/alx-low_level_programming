45d3b4c754b44f99e31898a03a215351923b648b
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-09 10:38:22 +0000

    add function that inserts a node at an index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a ne node at index idx
+ * @h: head of list
+ * @idx: index
+ * @n: data field
+ *
+ * Return: the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *save;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	if (!*h)
+	{
+		if (idx == 0)
+		{
+			new_node->prev = NULL;
+			new_node->next = NULL;
+			*h = new_node;
+			return (new_node);
+		}
+		return (NULL);
+	}
+	save = *h;
+	while (save)
+	{
+		if (count == idx)
+		{
+			new_node->next = (save->prev)->next;
+			(save->prev)->next = new_node;
+			new_node->prev = save->prev;
+			return (new_node);
+		}
+		save = save->next;
+		count++;
+	}
+	return (NULL);
+}
