0ec92629a27c2b2c57143e583afae7528d03f483
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-09 13:16:03 +0000

    fix code

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,9 +22,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 					(*head)->prev = NULL;
 				return (1);
 			}
+			if (save->next)
+				(save->next)->prev = save->prev;
 			(save->prev)->next = save->next;
-			/*if (save->next)*/
-				/*(save->next)->prev = save->prev;*/
 			return (1);
 		}
 		save = save->next;
