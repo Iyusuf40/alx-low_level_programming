3d6a85be638af02b8474ef7a1898afed7f73e154
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-09 13:45:07 +0000

    handle index 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,6 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
+		(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
