78b785ada23ab5f752fca883c7037a77eb06718f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-06-09 11:07:50 +0000

    fix head prev

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint - adds a new node at the end of list
+ * add_dnodeint - adds a new node at the beginning of list
  * @head: head of list
  * @n: element value of new node
  * Return: new node created
@@ -14,6 +14,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = *head;
+	if (*head)
+		*head->prev = new_node;
 	new_node->prev = NULL;
 	*head = new_node;
 	return (new_node);
