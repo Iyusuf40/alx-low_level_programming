e60042a0f79ddfc8f7c4bfc224f64cf1c8c38ff1
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-01 10:10:50 +0000

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		new_node->next = *address;
-		*address = new_node;
+		address = &new_node;
 	}
 	return (1);
 }
