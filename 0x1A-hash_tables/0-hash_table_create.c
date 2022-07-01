86ef91b70ef4087a9d8ade732568a658771905f4
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-01 12:44:12 +0000

    set array pointers to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
 	hash_node_t **array = NULL;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
@@ -23,6 +24,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 	new_table->array = array;
 	return (new_table);
 }
