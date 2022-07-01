f3d831238f4dd2a4ac5a8ab898b4f68a9bad6174
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-01 10:28:16 +0000

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->next = NULL;
 	if (!*address || sizeof(**address) != sizeof(hash_node_t))
 	{
-		address = &new_node;
+		*address = NULL;
 	}
 	else
 	{
