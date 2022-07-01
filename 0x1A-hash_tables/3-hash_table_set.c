de45ccf81ae0cdadcc8317c762349cbc916d391f
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-07-01 01:09:48 +0000

    test

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - sets nodes in a hash table
+ * @key: key to generate hash code
+ * @ht: hash table
+ * @value: value of key
+ *
+ * Return: 1 or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int hash_code, index;
+	hash_node_t *address = NULL, *head = NULL, *new_node;
+
+	hash_code = hash_djb2((unsigned char *)key);
+	index = key_index((unsigned char *)key, ht->size);
+
+	address = ht->array + index;
+
+	head = *address;
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		return (0);
+	}
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+	new_node->next = NULL;
+	if (!head)
+	{
+		head = new_node;
+	}
+	else
+	{
+		new_node->next = head;
+		head = new_node;
+	}
+	return (1);
+}
