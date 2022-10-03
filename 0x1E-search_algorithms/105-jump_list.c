fcda0513e89d0ed9adf11b31a0a43381d02a0b20
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-03 20:15:28 +0000

    add jump search implementation

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -9,19 +9,20 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t block = (size_t)sqrt(size), low, high, p;
+	size_t block = (size_t)sqrt(size), low, high = (size_t)sqrt(size), p, factor;
 	listint_t *x;
 
 	if (!list || !size)
 		return (NULL);
-
-	high = block;
+	factor = 32;
+	if (list->next)
+		factor = (size_t)((size_t)list - (size_t)(list->next));
 	low = 0;
-	x = list - 32;
+	x = list - factor;
 	while ((high < size) && (x->n <= value))
 	{
 		p = (size_t)list;
-		p = p - (32 * high);
+		p = p - (factor * high);
 		x = (listint_t *)p;
 		printf("Value checked at index [%lu] = [%d]\n", high, x->n);
 		if (x->n >= value || high == (size - 1))
@@ -34,14 +35,13 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	if (high >= size)
 		high = size - 1;
 	p = (size_t)list;
-	p = p - (32 * (high - 1));
+	p = p - (factor * (high - 1));
 	x = (listint_t *)p;
 	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
-
 	while (low <= high)
 	{
 		p = (size_t)list;
-		p = p - (32 * low);
+		p = p - (factor * low);
 		x = (listint_t *)p;
 		printf("Value checked at index [%lu] = [%d]\n", low, x->n);
 		if (x->n == value)
