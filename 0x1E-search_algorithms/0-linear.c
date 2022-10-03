57e86a6aa9db111d0e2f13fceb81c8c30ac732e5
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-03 10:10:44 +0000

    add readme, header and linear_search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for value in array
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ * Return: index value is found at or -1 if nof found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = 0;
+
+	if (!array)
+		return (-1);
+
+	while (index < (size - 1))
+	{
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return ((int)index);
+		index++;
+	}
+	return (-1);
+}
