317f82f1862d2ec58357cda0acc38875f5781cff
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-10 00:01:55 +0000

    rm redundant code

--- 0x1E-search_algorithms/bypass_pointer_arithmetic_demo.c
+++ 0x1E-search_algorithms/bypass_pointer_arithmetic_demo.c
@@ -6,29 +6,29 @@
  */
 int main(void)
 {
-	int x = 255, *x_address = &x, *p = &x;
-	size_t save = (size_t)p;
+	int x = 255, *x_address = &x;
+	size_t save = (size_t)x_address;
 
 	printf("x is originally %d\n", x);
-	save = save + 1;
-	p = (int *)save;
+	save = save + 1; /* 1 mem location above x address */
 	printf("converting to 65635.....\n");
 	*((unsigned char *)save + 0) = 255;
 	printf("now x is %d\n", x);
 	printf("converting to 62580.....\n");
-	*((char *)save - 1) = 0;
+	*((unsigned char *)save - 1) = 0;
 	printf("now x is %d\n", x);
 	printf("converting to 0.....\n");
-	*((char *)save) = 0;
+	*((unsigned char *)save) = 0;
 	printf("now x is %d\n", x);
 
 	printf("addresses: ");
-	printf("x_address = %lu p = %lu save = %lu\n", (size_t)x_address,
-			(size_t)p, save);
+	printf("x_address = %p save = %p\n", (void *)x_address, (void *)save);
+
 	printf("using pointer arithmetic x_address + 1...\n");
+
 	printf("addresses: ");
-	printf("x_address = %lu x_address++ = %lu p = %lu save = %lu\n",
-			(size_t)x_address, (size_t)(x_address + 1), (size_t)p, save);
+	printf("x_address = %p x_address++ = %p save = %p\n",
+			(void *)x_address, (void *)(x_address + 1), (void *)save);
 
 	return (0);
 }
